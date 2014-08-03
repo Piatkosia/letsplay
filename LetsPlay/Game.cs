b227ca2516a68d12e24bba1e7a1b1c5878ed3a48
piatkosia <piatkosia.apt@interia.pl>
2014-08-03 21:53:59 +0200

    wtf

---
+++ LetsPlay/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LetsPlay
+{
+    public class Game
+    {
+        public string gameName {get; set;}
+        public string pathFile { get; set; }
+    }
+}
---
+++ LetsPlay/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Management;
+
+namespace LetsPlay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Game> Gry;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2\\Applications\\Games",
+                    "SELECT * FROM Game"); 
+                Gry = new ObservableCollection<Game>();
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    
+                    Gry.Add(new Game{gameName = queryObj["Name"].ToString(), pathFile = queryObj["GDFBinaryPath"].ToString() });
+                }
+                listaGier.DataContext = Gry;
+            }
+            catch (ManagementException e)
+            {
+                MessageBox.Show("Coś nie poszło: " + e.Message);
+            }
+            
+        }
+    
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button cmd = (Button)sender;
+            if (cmd.DataContext is Game)
+            {
+                Game giera = (Game)cmd.DataContext;
+                System.Diagnostics.Process.Start(giera.pathFile);
+                okienko.WindowState = WindowState.Minimized;
+            }
+        }
+    }
+}
